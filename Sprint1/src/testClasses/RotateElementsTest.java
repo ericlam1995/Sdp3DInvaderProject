79735bfa4da66f2f6ff0b165b8579475d5e457b3
InderjitSJ <sgc8092@aut.ac.nz>
2016-08-29 13:51:31 +1200

    Changed package name New test package

---
+++ Sprint1/src/testClasses/RotateElementsTest.java
@@ -0,0 +1,14 @@
+package testClasses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RotateElementsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
